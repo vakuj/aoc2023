b3e3bc1e4a22dfdabd96265dd4479e80f90c18b0
vakuj <vakujrvi@gmail.com>
2023-12-23 10:39:57 +0100

    day 1 part 2 done. made some improvement to layout and logging of results

--- src/d1.rs
+++ src/d1.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 
-pub fn part1(file_path: &str) -> u32 {
+pub fn part1(file_path: String) -> u32 {
     let mut path = std::env::current_dir().unwrap();
     path.push(PathBuf::from(file_path));
 
@@ -35,11 +35,68 @@ pub fn part1(file_path: &str) -> u32 {
     return sum;
 }
 
+const NUMBERS: &'static [&'static str] = &[
+    "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7",
+    "eight", "8", "nine", "9",
+];
+
+fn str_to_number(input: &str) -> u32 {
+    match input {
+        "one" | "1" => 1u32,
+        "two" | "2" => 2u32,
+        "three" | "3" => 3u32,
+        "four" | "4" => 4u32,
+        "five" | "5" => 5u32,
+        "six" | "6" => 6u32,
+        "seven" | "7" => 7u32,
+        "eight" | "8" => 8u32,
+        "nine" | "9" => 9u32,
+        _ => 0u32,
+    }
+}
+pub fn part2(file_path: String) -> u32 {
+    let mut path: PathBuf = std::env::current_dir().unwrap();
+    path.push(PathBuf::from(file_path));
+
+    let mut results: Vec<u32> = Vec::<u32>::new();
+    fs::read_to_string(path.as_path())
+        .unwrap()
+        .lines()
+        .map(String::from)
+        .into_iter()
+        .for_each(|elem| {
+            let mut name_matches: Vec<_> = Vec::<(usize, &str)>::new();
+            NUMBERS.into_iter().for_each(|number| {
+                elem.match_indices(number).for_each(|item| {
+                    name_matches.push(item);
+                });
+            });
+            name_matches.sort_by_key(|key| key.0);
+            results.push(
+                match name_matches.first() {
+                    Some(r) => str_to_number(r.1) * 10,
+                    None => 0,
+                } + match name_matches.last() {
+                    Some(r) => str_to_number(r.1),
+                    None => 0,
+                },
+            );
+        });
+    let mut sum = 0u32;
+    results.into_iter().for_each(|r| {
+        sum += r;
+    });
+    return sum;
+}
+
 #[cfg(test)]
 mod test_d1 {
-    use super::part1;
     #[test]
     pub fn test_d1_p1() {
-        assert_eq!(part1("data/test_d1p1.txt"), 142);
+        assert_eq!(super::part1(String::from("data/d1/test_p1.txt")), 142);
+    }
+    #[test]
+    pub fn test_d1_p2() {
+        assert_eq!(super::part2(String::from("data/d1/test_p2.txt")), 281);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
+use std::env;
 #[allow(unused)]
 use std::fs;
 #[allow(unused)]
 use std::path::{Path, PathBuf};
+use std::{fs::OpenOptions, io::Write};
 
 pub mod d1;
 
 fn main() {
-    let d1p1 = d1::part1("data/input_d1.txt");
-    println!("Part 1: {d1p1}");
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Supply a day number to run");
+        return;
+    }
+
+    let day_arg = &args[1];
+    let day: u32 = match day_arg.parse() {
+        Ok(d) => d,
+        Err(_err) => {
+            println!("Could not parse input argument number {day_arg} : {_err}");
+            0
+        }
+    };
+
+    let result_str: String;
+    match day {
+        1 => {
+            let p1 = d1::part1(format!("data/d{day}/input.txt"));
+            let p2 = d1::part2(format!("data/d{day}/input.txt"));
+            result_str = format!("Day {}\nPart 1: {}\nPart 2: {}\n", day, p1, p2);
+        }
+        _ => return,
+    }
+    let mut output_path = env::current_dir().unwrap();
+
+    let output_file = format!("output/output{day}.txt");
+    output_path.push(PathBuf::from(output_file));
+
+    println!("{result_str}");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open(output_path.as_path())
+        .unwrap();
+    file.write_all(result_str.as_bytes()).unwrap();
+    println!("Task done");
 }
