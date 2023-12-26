9d09b89b3fff54186a1a2b2778dbe2c32eadf0ac
vakuj <vakujrvi@gmail.com>
2023-12-26 12:01:33 +0100

    d4 part 2 done

--- src/d4.rs
+++ src/d4.rs
@@ -6,6 +6,7 @@ use std::path::PathBuf;
 struct Card {
     nr: i32,
     correct: Vec<i32>,
+    multiplier: u32,
 }
 
 impl Card {
@@ -13,6 +14,7 @@ impl Card {
         Card {
             nr: 0,
             correct: Vec::<i32>::new(),
+            multiplier: 1,
         }
     }
 
@@ -56,19 +58,42 @@ pub fn part1(file_path: String) -> u32 {
         .for_each(|line| cards.push(Card::from_string(line)));
 
     let mut sum = 0u32;
-
     cards.into_iter().for_each(|card| {
         if card.correct.len() >= 1 {
             sum += 1 << (card.correct.len() - 1);
         }
-        println!("{:?}", card);
+        // println!("{:?}", card);
     });
 
     return sum;
 }
 
 pub fn part2(file_path: String) -> u32 {
-    0
+    let mut path = std::env::current_dir().unwrap();
+    path.push(PathBuf::from(file_path));
+
+    let mut cards = Vec::<Card>::new();
+    fs::read_to_string(path.as_path())
+        .expect("Could not open file")
+        .lines()
+        .for_each(|line| cards.push(Card::from_string(line)));
+
+    let mut sum = 0u32;
+    for ii in 0..cards.len() {
+        let wins = cards[ii].correct.len();
+        let upper = ii + wins + 1;
+        let upper = if upper <= cards.len() {
+            upper
+        } else {
+            cards.len()
+        };
+        for jj in ii + 1..upper {
+            cards[jj].multiplier += cards[ii].multiplier;
+        }
+        sum += cards[ii].multiplier;
+        // println!("{:?}: +{:?} = {:?}", ii, cards[ii].multiplier, sum);
+    }
+    return sum;
 }
 
 #[cfg(test)]
@@ -78,5 +103,10 @@ mod test_d4 {
     pub fn test_d4_p1() {
         let a = super::part1(String::from("data/d4/test_p1.txt"));
         assert_eq!(a, 13);
+    }
+    #[test]
+    pub fn test_d4_p2() {
+        let a = super::part2(String::from("data/d4/test_p1.txt"));
+        assert_eq!(a, 30);
     }
 }
