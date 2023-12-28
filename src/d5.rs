18e9e4076fb3c009c6c9dfe09aa4d6102af3f70e
vakuj <vakujrvi@gmail.com>
2023-12-28 21:41:53 +0100

    d5 part2: passes test, finds a solution to real input with little delay but is too high

--- src/d5.rs
+++ src/d5.rs
@@ -3,18 +3,24 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::Error;
 use std::path::PathBuf;
-
+/// the map type used key pairs to map items
 type Map = HashMap<(MapType, MapType), Vec<MapItem>>;
 #[derive(Debug)]
+/// map item with start of destination and source range and the length of the ranges
 struct MapItem {
+    /// destination start
     dst: usize,
+    /// source start
     src: usize,
+    /// length of range
     len: usize,
 }
-#[derive(Debug)]
-struct SeedRange(usize, usize);
+#[derive(Debug, Clone, Copy)]
+/// seed range type with start and end
+struct ItemRange(usize, usize);
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+/// map type keys, combine to pairs to use in map
 enum MapType {
     Seed,
     Soil,
@@ -26,14 +32,23 @@ enum MapType {
     Location,
     Unknown,
 }
+
+/// a record of the keys in the order as they appear
 static mut KEY_ORDER: Vec<(MapType, MapType)> = Vec::<(MapType, MapType)>::new();
+
+/// starting seeds for part 1
 static mut SEEDS_P1: Vec<usize> = Vec::<usize>::new();
-static mut SEEDS_P2: Vec<SeedRange> = Vec::<SeedRange>::new();
+
+/// starting seed ranges for part 2
+static mut SEEDS_P2: Vec<ItemRange> = Vec::<ItemRange>::new();
+
+/// the mapping from keys to map items
 static mut MAPPING: Lazy<Map> = Lazy::new(|| {
     let mut map = Map::new();
     let _ = map.insert((MapType::Unknown, MapType::Unknown), Vec::<MapItem>::new());
     map
 });
+/// flag indicating data is loaded
 static mut DATA_LOADED: bool = false;
 
 impl MapItem {
@@ -52,6 +67,25 @@ impl MapItem {
             return Some(self.dst + (src - self.src));
         }
         None
+    }
+    pub fn get_dst_range(&self, src: &ItemRange) -> Option<ItemRange> {
+        // src: (46..56) ,self.src:
+
+        if self.src + self.len < src.0 || src.1 <= self.src {
+            return None;
+        }
+        let offset = if self.src > src.0 {
+            0
+        } else {
+            src.0 - self.src
+        };
+        let length = src.1 - src.0 - 1;
+        let length = if self.len < length { self.len } else { length };
+
+        return Some(ItemRange {
+            0: self.dst + offset,
+            1: self.dst + offset + length,
+        });
     }
 }
 impl MapType {
@@ -139,7 +173,7 @@ pub fn reload_seeds(file_path: String) -> Result<(), Error> {
 
     for ii in (0..unsafe { SEEDS_P1.len() }).step_by(2) {
         unsafe {
-            SEEDS_P2.push(SeedRange {
+            SEEDS_P2.push(ItemRange {
                 0: SEEDS_P1[ii],
                 1: SEEDS_P1[ii] + SEEDS_P1[ii + 1],
             });
@@ -193,43 +227,42 @@ pub fn part2(file_path: String) -> usize {
     reload_seeds(file_path).expect("Could not reload seeds");
 
     let mut best_location: Option<usize> = None;
-    let mut next_src: usize;
-    for sr in unsafe { &SEEDS_P2 } {
-        for seed in sr.0..sr.1 {
-            next_src = seed;
-            for key in unsafe { &KEY_ORDER } {
-                // panic if key not found, should be there...
-                if unsafe { !MAPPING.contains_key(&key) } {
-                    panic!("key not found");
-                }
-                // try to find next destination by evaluating the sources of
-                // the items related to this key. if not found the source and
-                // destination are mapped to the same value.
-                for item in unsafe { MAPPING.get(&key).unwrap() } {
-                    if let Some(ns) = item.get_dst(next_src) {
-                        println!("{key:?} : {next_src:?} -> {ns:?}");
-                        next_src = ns;
-                        break;
-                    }
-                }
+    let mut next_src: ItemRange;
+    for seed in unsafe { &SEEDS_P2 } {
+        next_src = *seed;
+        for key in unsafe { &KEY_ORDER } {
+            // panic if key not found, should be there...
+            if unsafe { !MAPPING.contains_key(&key) } {
+                panic!("key not found");
             }
 
-            if let Some(bl) = best_location {
-                if bl > next_src {
-                    best_location = Some(next_src);
+            for item in unsafe { MAPPING.get(&key).unwrap() } {
+                if let Some(ns) = item.get_dst_range(&next_src) {
+                    // println!("{key:?} : {next_src:?} -> {ns:?}");
+                    next_src = ns;
+                    break;
                 }
-            } else {
-                best_location = Some(next_src);
             }
-            println!("---- {best_location:?} [{sr:?})");
+            println!("{key:?} : {next_src:?}");
         }
+
+        if let Some(bl) = best_location {
+            if bl > next_src.0 {
+                best_location = Some(next_src.0);
+            }
+        } else {
+            best_location = Some(next_src.0);
+        }
+        println!("---- {best_location:?} [{seed:?})");
     }
     return best_location.unwrap();
 }
 
 #[cfg(test)]
 mod test_d5 {
-    use crate::d5::{load_data, reload_seeds, MapType, KEY_ORDER, SEEDS_P1, SEEDS_P2};
+    use crate::d5::{
+        load_data, reload_seeds, MapType, DATA_LOADED, KEY_ORDER, MAPPING, SEEDS_P1, SEEDS_P2,
+    };
 
     #[test]
     pub fn test_d5_load() {
@@ -263,5 +296,21 @@ mod test_d5 {
     pub fn test_d5_p2() {
         let a = super::part2(String::from("data/d5/test_p1.txt"));
         assert_eq!(a, 46);
+        //  too high on real input
+    }
+    #[test]
+    pub fn test_d5_p2real() {
+        unsafe {
+            if DATA_LOADED {
+                // clear data if loaded in previous tests
+                DATA_LOADED = false;
+                KEY_ORDER.clear();
+                MAPPING.clear();
+                SEEDS_P1.clear();
+                SEEDS_P2.clear();
+            }
+        }
+        let a = super::part2(String::from("data/d5/input.txt"));
+        assert!(a < 283658805);
     }
 }
