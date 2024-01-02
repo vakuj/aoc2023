79ce254cfa7681546c4dde39767cf3e87e716227
vakuj <vakujrvi@gmail.com>
2024-01-02 18:34:24 +0100

    reworked d6p1 and started on p2

--- src/d6.rs
+++ src/d6.rs
@@ -39,24 +39,40 @@ pub fn load_data(file_path: String) -> Result<(Vec<u32>, Vec<u32>), String> {
     }
     Ok((time, dist))
 }
-fn tot_dist(tot_time: u32, acc_time: u32) -> u32 {
-    if tot_time == 0 || acc_time == 0 || acc_time == tot_time {
-        return 0;
+
+fn load_data_p2(file_path: String) -> Result<(u32, u32), String> {
+    let mut path = std::env::current_dir().unwrap();
+    path.push(PathBuf::from(file_path));
+    let binding = fs::read_to_string(path).expect("Should load file");
+    let lines = binding.lines().collect::<Vec<&str>>();
+    if lines.len() != 2 {
+        return Err(format!("Expected to read 2 lines got {}", lines.len()));
     }
-    let mut vel = 0u32;
-    let mut dist = 0u32;
     let mut time = 0u32;
-    let mut acc = 0u32;
-    while tot_time > time {
-        if acc_time > acc {
-            acc += 1;
-        } else {
-            vel = acc;
+    let mut dist = 0u32;
+    if let Some((_, x)) = lines[0].trim().split_once(':') {
+        if let Ok(y) = x
+            .trim()
+            .split(' ')
+            .flat_map(|s| s.chars())
+            .collect::<String>()
+            .parse()
+        {
+            time = y;
         }
-        dist += vel;
-        time += 1;
     }
-    return dist;
+    if let Some((_, x)) = lines[1].trim().split_once(':') {
+        if let Ok(y) = x
+            .trim()
+            .split(' ')
+            .flat_map(|s| s.chars())
+            .collect::<String>()
+            .parse()
+        {
+            dist = y;
+        }
+    }
+    Ok((time, dist))
 }
 
 pub fn part1(file_path: String) -> u32 {
@@ -70,21 +86,26 @@ pub fn part1(file_path: String) -> u32 {
         Err(str) => panic!("{str}"),
     }
 
-    let mut result = 0u32;
+    let mut result = 1u32;
 
-    for (time, dist) in zip(times, dists) {
+    zip(times, dists).for_each(|(time, dist)| {
+        println!("{time} - {dist}");
         let mut ctr = 0u32;
-        for acc_time in 0..time {
-            if tot_dist(time, acc_time) > dist {
-                ctr += 1;
-            }
-        }
-        if result == 0 {
-            result = ctr;
-        } else {
-            result *= ctr;
-        }
-    }
+        let _ = (0..time)
+            .scan(0, |vel, t| {
+                // println!("{t} = {vel}");
+                let d = (time - t) * *vel;
+                if d > dist {
+                    ctr += 1;
+                    println!("{d} = {vel}");
+                }
+                *vel = *vel + 1;
+                Some(*vel)
+            })
+            .collect::<Vec<u32>>();
+        result *= ctr;
+    });
+
     println!("{result}");
     result
 }
@@ -95,7 +116,7 @@ pub fn part2(file_path: String) -> u32 {
 
 #[cfg(test)]
 mod test_d6 {
-    use crate::d6::load_data;
+    use crate::d6::{load_data, load_data_p2};
 
     #[test]
     pub fn load_test_d6_p1() {
@@ -117,6 +138,20 @@ mod test_d6 {
         assert_eq!(dist[1], 40);
         assert_eq!(dist[2], 200);
     }
+    #[test]
+    pub fn load_test_d6_p2() {
+        let time: u32;
+        let dist: u32;
+        match load_data_p2(String::from("data/d6/test_p1.txt")) {
+            Ok(data) => {
+                time = data.0;
+                dist = data.1;
+            }
+            Err(str) => panic!("{str}"),
+        }
+        assert_eq!(time, 71530);
+        assert_eq!(dist, 940200);
+    }
 
     #[test]
     pub fn test_d6_p1() {
@@ -124,6 +159,7 @@ mod test_d6 {
         assert_eq!(a, 288);
     }
     #[test]
+    #[ignore = "reason"]
     pub fn test_d6_p1real() {
         let a = super::part1(String::from("data/d6/input.txt"));
         assert_eq!(a, 227850);
