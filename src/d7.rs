71d1f6eb3ca6b88d7abcb32a349c3a751866d9f8
vakuj <vakujrvi@gmail.com>
2024-01-08 22:50:22 +0100

    d7 p1 done

--- src/d7.rs
+++ src/d7.rs
@@ -1,38 +1,100 @@
 use core::panic;
+use std::cmp::Ordering;
 use std::fs;
 use std::path::PathBuf;
 
 #[derive(Debug)]
-pub struct Hand<'a> {
-    cards: &'a mut [Option<Card>; 5],
+pub struct Hand {
+    cards: Vec<u32>,
     bid: u32,
-    index: usize,
+    rank: Rank,
 }
-#[derive(Debug)]
-pub struct Card {
-    value: u32,
-    count: u32,
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum Rank {
+    Undefined = 0,
+    HighCard = 1,
+    OnePair = 2,
+    TwoPair = 3,
+    ThreeOfKind = 4,
+    FullHouse = 5,
+    FourOfKind = 6,
+    FiveOfKind = 7,
 }
-impl<'a> Hand<'a> {
+impl Hand {
     pub fn new() -> Self {
         Hand {
-            cards: &mut [None, None, None, None, None],
+            cards: Vec::<u32>::new(),
             bid: 0,
-            index: 0,
+            rank: Rank::Undefined,
         }
     }
 
-    pub fn from_string(line: String) -> Option<Self> {
+    pub fn from_string(line: &str) -> Option<Self> {
         if let Some((a, b)) = line.trim().split_once(' ') {
             let mut hand = Hand::new();
-
+            let mut uhand = Vec::<(u32, u32)>::new();
             a.chars().for_each(|c| {
-                match Card::from_char(c) {
-                    Some(card) => hand.insert(&card),
-                    None => panic!("Could not parse {c} as a card"),
+                let value: u32 = match c {
+                    'A' => 14,
+                    'K' => 13,
+                    'Q' => 12,
+                    'J' => 11,
+                    'T' => 10,
+                    '9' => 9,
+                    '8' => 8,
+                    '7' => 7,
+                    '6' => 6,
+                    '5' => 5,
+                    '4' => 4,
+                    '3' => 3,
+                    '2' => 2,
+                    _ => panic!("Could not parse {c} as a card"),
                 };
-            });
 
+                hand.cards.push(value);
+                let mut inserted = false;
+                for ii in 0..uhand.len() {
+                    if uhand[ii].0 == value {
+                        uhand[ii].1 += 1;
+                        inserted = true;
+                        break;
+                    }
+                }
+                if !inserted {
+                    uhand.push((value, 1));
+                }
+            });
+            match uhand.len() {
+                1 => hand.rank = Rank::FiveOfKind,
+                2 => {
+                    if uhand[0].1 == 4 || uhand[1].1 == 4 {
+                        hand.rank = Rank::FourOfKind;
+                    } else {
+                        hand.rank = Rank::FullHouse;
+                    }
+                }
+                3 => {
+                    // 2x2 + 1 or  1x3+1+1
+                    if uhand[0].1 == 3 || uhand[1].1 == 3 || uhand[2].1 == 3 {
+                        hand.rank = Rank::ThreeOfKind;
+                    } else {
+                        hand.rank = Rank::TwoPair;
+                    }
+                }
+                4 => {
+                    // 1x2+1+1+1
+                    hand.rank = Rank::OnePair;
+                }
+                5 => {
+                    // 1+1+1+1+1
+                    hand.rank = Rank::HighCard;
+                }
+                _ => {
+                    // None where set
+                    hand.rank = Rank::Undefined;
+                }
+            }
             match b.parse() {
                 Ok(bid) => hand.bid = bid,
                 Err(err) => panic!("Could not parse {b} as bid: {err}"),
@@ -42,105 +104,23 @@ impl<'a> Hand<'a> {
         None
     }
 
-    pub fn contains(self, other: &Card) -> Option<usize> {
+    pub fn cmp(&self, other: &Hand) -> Ordering {
+        if self.rank > other.rank {
+            return Ordering::Greater;
+        }
+        if self.rank < other.rank {
+            return Ordering::Less;
+        }
         for ii in 0..5 {
-            if let Some(card) = self.cards[ii] {
-                if card.value == other.value {
-                    return Some(ii);
-                }
+            if self.cards[ii] == other.cards[ii] {
+                continue;
             }
+            if self.cards[ii] > other.cards[ii] {
+                return Ordering::Greater;
+            }
+            return Ordering::Less;
         }
-        None
-    }
-
-    pub fn insert(&mut self, other: &Card) {
-        match self.contains(other) {
-            Some(index) => {
-                if let Some(card) = &self.cards[index] {
-                    card.incr_count();
-                }
-            }
-            None => {
-                if self.index >= 5 {
-                    panic!(
-                        "Out-of-bound insert of card attempted @ index {}",
-                        self.index
-                    );
-                }
-                self.cards[self.index] = Some(*other);
-                self.index += 1;
-            }
-        }
-    }
-
-    pub fn rank(&self) -> u32 {
-        match self.cards {
-            [Some(card1), None, None, None, None] => {
-                // 1x5
-                7
-            }
-            [Some(card1), Some(card2), None, None, None] => {
-                // 1x4+1 or (1x3+1x2)
-                if card1.count == 4 || card2.count == 4 {
-                    6
-                } else {
-                    5
-                }
-            }
-            [Some(card1), Some(card2), Some(card3), None, None] => {
-                // 2x2 + 1 or  1x3+1+1
-                if card1.count == 3 || card2.count == 3 || card3.count == 3 {
-                    3
-                } else {
-                    4
-                }
-            }
-            [Some(card1), Some(card2), Some(card3), Some(card4), None] => {
-                // 1x2+1+1+1
-                2
-            }
-            [Some(card1), Some(card2), Some(card3), Some(card4), Some(card5)] => {
-                // 1+1+1+1+1
-                1
-            }
-            _ => {
-                // None where set
-                0
-            }
-        }
-    }
-}
-
-impl Card {
-    pub fn new(value: u32) -> Self {
-        Card { value, count: 0 }
-    }
-
-    pub fn from_char(c: char) -> Option<Self> {
-        let value: u32 = match c {
-            'A' => 14,
-            'K' => 13,
-            'Q' => 12,
-            'J' => 11,
-            'T' => 10,
-            '9' => 9,
-            '8' => 8,
-            '7' => 7,
-            '6' => 6,
-            '5' => 5,
-            '4' => 4,
-            '3' => 3,
-            '2' => 2,
-            _ => 0,
-        };
-        if value != 0 {
-            return Some(Card::new(value));
-        }
-        None
-    }
-
-    pub fn incr_count(&mut self) {
-        self.count += 1;
+        Ordering::Equal
     }
 }
 
@@ -150,12 +130,26 @@ pub fn load_data(file_path: String) -> Result<Vec<Hand>, String> {
     let binding = fs::read_to_string(path).expect("Should load");
     let lines = binding.lines().collect::<Vec<&str>>();
     let mut hands = Vec::<Hand>::new();
-
+    lines.into_iter().for_each(|line| {
+        if let Some(hand) = Hand::from_string(line) {
+            hands.push(hand)
+        }
+    });
+    // hands.iter().for_each(|hand| println!("{hand:?}"));
     Ok(hands)
 }
 
 pub fn part1(file_path: String) -> u32 {
-    0
+    match load_data(file_path) {
+        Ok(mut hands) => {
+            hands.sort_by(|a, b| a.cmp(b));
+            return hands
+                .iter()
+                .enumerate()
+                .fold(0u32, |acc, (ii, hand)| acc + hand.bid * (ii as u32 + 1));
+        }
+        Err(str) => panic!("{str}"),
+    }
 }
 
 pub fn part2(file_path: String) -> usize {
@@ -164,19 +158,47 @@ pub fn part2(file_path: String) -> usize {
 
 #[cfg(test)]
 mod test_d7 {
-    use crate::d7::load_data;
+    use crate::d7::{load_data, Rank};
 
     #[test]
     pub fn load_test_d7_p1() {
         match load_data(String::from("data/d7/test_p1.txt")) {
-            Ok(data) => {}
+            Ok(data) => {
+                assert_eq!(data[0].bid, 765);
+                assert_eq!(data[1].bid, 684);
+                assert_eq!(data[2].bid, 28);
+                assert_eq!(data[3].bid, 220);
+                assert_eq!(data[4].bid, 483);
+
+                assert_eq!(data[0].rank, Rank::OnePair);
+                assert_eq!(data[1].rank, Rank::ThreeOfKind);
+                assert_eq!(data[2].rank, Rank::TwoPair);
+                assert_eq!(data[3].rank, Rank::TwoPair);
+                assert_eq!(data[4].rank, Rank::ThreeOfKind);
+
+                assert_eq!(data[0].cards[0], 3);
+                assert_eq!(data[0].cards[1], 2);
+                assert_eq!(data[0].cards[2], 10);
+                assert_eq!(data[0].cards[3], 3);
+                assert_eq!(data[0].cards[4], 13);
+
+                assert_eq!(data[1].cards[0], 10);
+                assert_eq!(data[2].cards[0], 13);
+                assert_eq!(data[3].cards[0], 13);
+                assert_eq!(data[4].cards[0], 12);
+            }
             Err(str) => panic!("{str}"),
         }
     }
 
     #[test]
-    pub fn test_d6_p1() {
+    pub fn test_d7_p1() {
         let a = super::part1(String::from("data/d7/test_p1.txt"));
         assert_eq!(a, 6440);
+    }
+    #[test]
+    pub fn test_d7_p1real() {
+        let a = super::part1(String::from("data/d7/input.txt"));
+        assert_eq!(a, 0);
     }
 }
